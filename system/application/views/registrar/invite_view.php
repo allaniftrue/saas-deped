5ec54e96de00028f4509b52db5ecdf305d8f2c8e
paparts <icpep.org@gmail.com>
2012-11-13 23:58:09 +0800

    change preloader to BS button

--- system/application/views/registrar/invite_view.php
+++ system/application/views/registrar/invite_view.php
@@ -5,20 +5,16 @@ if($this->session->userdata('usertype') === 'registrar') {
     $this->load->view('teacher/main_header_view');
 }?>
 <section class="span11">
-     <div class="page-header">
-            <h1>Invitation</h1>
-     </div>
- 
+    <h2>Invitation</h2>
     <div class="span5">
         <div class="hide" id="stat">
             
         </div>
-        
-         <form id="invitation-form" method="post" action="">
+        <form id="invitation-form" method="post" action="">
             <div style="margin:0px 0px 12px 0px;padding:5px 0;border-bottom: 1px dotted #ccc;" id="drop-shadow-bottom">
             <a href="javascript:void(0);" id="add-field"><img src="<?=base_url()?>img/plus-white.png" /> Add more fields</a>
             </div>
-            <label>Teacher's Email Address</label>
+            <label class="required"><strong>Teacher's Email Address</strong></label>
             <div id="field-wrap">
             <p>
                 <label for="email"></label>
@@ -29,13 +25,30 @@ if($this->session->userdata('usertype') === 'registrar') {
                 <button type="submit" class="btn btn-primary" id="send-invitation" data-loading-text="Sending Invitation...">
                     <i class="icon-envelope icon-white"></i> Send Invitation
                 </button>
-                <img src="<?=base_url()?>img/ajax-loader.gif" style="display:none;" id="preloader" />
             </p>
         </form>
     </div>
     <div class="span5">
-            <div class="alert alert-info"><h3>Invite a faculty member</h3></div>
-                <p align="justify">This allow registered teachers to invite co-teachers in their respective institution.  All invites are tracked and you could be liable for inviting anybody not from your school.</p>
+        
+        <div class="popover right" id="popover">
+            <div class="arrow"></div>
+            <h2 class="popover-title"><strong>Sending Invitations</strong></h2>
+            <div class="popover-content">
+              <h4>Faculty and Staff</h4>
+              <p align="justify">Allows registered teachers to invite co-teachers in their respective institution.  The invitation allows the owner of the email to access data provided for teachers.</p><br />
+              
+              <h4>DepEd Officials</h4>
+              <p align="justify">
+                  Allows the registrar to invite DepEd officials who are assigned to check school reports.  The invitation allows the owner of the email to access data provided for DepEd officials.
+              </p> <br />
+              
+              <div class="alert alert-error">
+                  <p align="justify">
+                  <i class="icon-exclamation-sign"></i> All invites are tracked and you could be liable for inviting anybody not from your school.
+                  </p>
+              </div>
+            </div>
+        </div>
    </div>
 </section>
 <div class="modal hide" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -51,22 +64,29 @@ if($this->session->userdata('usertype') === 'registrar') {
 </div>
 <script src="<?=base_url()?>js/bootstrap-dropdown.js"></script>
 <script src="<?=base_url()?>js/bootstrap-button.js"></script>
+<script src="<?=base_url()?>js/bootstrap-tooltip.js"></script>
+<script src="<?=base_url()?>js/bootstrap-popover.js"></script>
 <script src="<?=base_url()?>js/bootstrap-modal.js"></script>
-<!--<script src="<?=base_url()?>js/jquery-ui.min.js"></script>-->
 <script src="<?=base_url()?>js/jquery.validate.min.js"></script>
 <script src="<?=base_url()?>js/jquery.metadata.js"></script>
 <script src="<?=base_url()?>js/application.js"></script>
+<script src="<?=base_url()?>js/jquery.masonry.min.js"></script>
 
 <script type="text/javascript">
 $(document).ready(function(){
-     var varsVal = getUrlVars()['act']; 
-     $('#add-field').click(function(){
-        $('#field-wrap').prepend('<p><input type="email" name="email[]" id="email" class="input-xlarge" validate="required:true,email: true" /></p>');
-     });
-       $("#invitation-form").validate({
+
+    var varsVal = getUrlVars()['act']; 
+    
+    $('#add-field').click(function(){
+        $('#field-wrap').prepend('<p><input type="email" name="email[]" id="email" class="input-xlarge" validate="required:true,email: true" /></p>')
+                        .masonry('reload');
+    });
+    
+    $("#invitation-form").validate({
                 meta: "validate",
+                errorPlacement: function(error, element) {element.css('border','1px solid #B94A48').attr('placeholder','Enter email address')},
                 submitHandler: function() {
-                    $('#preloader').show();
+                    $('button').button('loading')
                     var outArr =[];
                     var emailArr=document.getElementsByName('email[]'); 
                     var arrLn = emailArr.length;
@@ -82,9 +102,8 @@ $(document).ready(function(){
                                 data:{email:curEmail,utype:varsVal},
                                 dataType:'json',
                                 success:function(msg){
-                                    console.log(msg);                                 
-                                    $('#stat').show('slow');
-                                    var $this = $('#stat');
+                                    $('#stat').show('slow')
+                                    var $this = $('#stat')
                                     if(msg.status == 1) {
                                         $this.append('<div class="alert alert-success">'+msg.msg+'</div>');
                                     } else {
@@ -92,19 +111,32 @@ $(document).ready(function(){
                                     }
                                     setTimeout(function () {
                                         $this.slideUp('slow').empty('slow');
-                                    }, 4000);
-                                    $('#preloader').hide();
+                                    }, 4000)
+                                    $('button').button('reset')
                                 },
                                 error:function(xhr,ajaxOptions,thrownError){
-                                    $('#preloader').hide();
-                                    $('.modal-body').empty().append("<p>"+'xhr status: '+xhr.status + '\n Error:'+ thrownError+"</p>");
-                                    $('#myModal').modal('show');
+                                    $('.modal-body').empty().append("<p>"+'xhr status: '+xhr.status + '\n Error:'+ thrownError+"</p>")
+                                    $('#myModal').modal('show')
+                                    $('button').button('reset')
                                 }
                             });
                         }
-                    }  $("#field-wrap").load(location.href + " #field-wrap");
+                    }  
+                    $("#field-wrap").load(location.href + " #field-wrap")
+                    
                 }
      });    
+     
+     $('#field-wrap').masonry({
+            isAnimated: true,
+            animationOptions: {
+            duration: 100,
+            easing: 'linear',
+            queue: false
+        }
+     })
+     
+     
 });
 </script>
 <?php $this->load->view('registrar/footer_view'); ?>
--- system/application/views/registrar/myinvites_view.php
+++ system/application/views/registrar/myinvites_view.php
@@ -5,13 +5,7 @@ if($this->session->userdata('usertype') === 'registrar') {
     $this->load->view('teacher/main_header_view');
 } else { redirect(base_url(), 'location'); }?>
 <section class="span11">
-    <div class="page-header">
-            <h1>List of Users Invited</h1>
-    </div>
-    <div class="alert alert-info">
-        <strong>Notes:</strong> Expired invitations are not shown
-    </div>
-    
+    <h2>List of Users Invited</h2>    
     <table class="table table-striped table-bordered table-condensed">
         <thead>
             <tr>
@@ -63,14 +57,16 @@ if($this->session->userdata('usertype') === 'registrar') {
                         } else {
                     ?>
                             <div class="btn-group">
-                                <button class="btn btn-mini" data-toggle="dropdown">Actions</button>
-                                <button data-toggle="dropdown" class="btn btn-mini dropdown-toggle"><span class="caret"></span></button>
+                                <button class="btn btn-mini" data-toggle="dropdown" data-loading-text="Sending..." id="action<?=$list[$i]->inv_user?>">Actions</button>
+                                <button class="btn btn-mini dropdown-toggle" data-loading-text="" id="action<?=$list[$i]->inv_user?>"><span class="caret"></span></button>
                                 <ul class="dropdown-menu">
                                     <li>
                                         <a href="javascript:void(0);" id="rm" data-id="<?=$list[$i]->inv_user?>" title="Remove this invitation"><i class="icon-trash"></i> Remove invitation</a>
                                     </li>
                                     <li>
-                                        <a href="javascript:void(0);" id="resend" data-id="<?=$list[$i]->inv_user?>" title="Resend the activation link"><i class="icon-envelope"></i> Resend invitation</a>
+                                        <a href="javascript:void(0);" id="resend" data-id="<?=$list[$i]->inv_user?>" title="Resend the activation link">
+                                            <i class="icon-envelope"></i> Resend Invitation
+                                        </a>
                                     </li>
                                 </ul><img src="<?=base_url()?>img/ajax-loader.gif" class="hide" id="img<?=$list[$i]->inv_user?>" />
                             </div>
@@ -141,6 +137,7 @@ if($this->session->userdata('usertype') === 'registrar') {
 <script src="<?=base_url()?>js/bootstrap-modal.js"></script>
 <script src="<?=base_url()?>js/jquery.validate.min.js"></script>
 <script src="<?=base_url()?>js/jquery-ui.min.js"></script>
+<script src="<?=base_url()?>js/bootstrap-button.js"></script>
 <script src="<?=base_url()?>js/jquery.tipsy.js"></script>
 <script src="<?=base_url()?>js/application.js"></script>
 <script type="text/javascript">
@@ -151,10 +148,9 @@ $(document).ready(function(){
        var $data_id = $(this).attr('data-id');
        var conf = confirm('Are you sure you want to remove this invitation?');
        
-       $("#img"+$data_id).show();
-       
        if(conf === true) {
-           
+           var $button = $('#action'+$data_id)
+           $button.button('loading')
            $.ajax({
                 type:'post',
                 url:'?mc=invite&m=rm',
@@ -169,15 +165,14 @@ $(document).ready(function(){
                         $('#modal01').empty().append('<p>'+json.msg+'</p>');
                     }
                     
-                    $('#myModal01').modal('show');
-                    $("#img"+$data_id).hide();
+                    $('#myModal01').modal('show')
+                    $button.button('reset')
                     
                 },           
                 error:function (xhr, ajaxOptions, thrownError){
-                        $('#modal01').empty().append('<p>There was an error in removing the invitation.'+thrownError+'</p>');
-                        $('#myModal01').modal('show');
-                        $("#img"+$data_id).hide();
-                        
+                        $('#modal01').empty().append('<p>There was an error in removing the invitation.'+thrownError+'</p>')
+                        $('#myModal01').modal('show')
+                        $button.button('reset')
                 } 
             });    
            $('tr#'+$data_id).hide();
@@ -187,10 +182,11 @@ $(document).ready(function(){
     });
     
     $('#resend').live("click",function(){
-        
-        var $data_id = $(this).attr('data-id');
-        
-        $("#img"+$data_id).show();
+       
+        var $data_id = $(this).attr('data-id')
+        var $button = $('#action'+$data_id)
+        $button.button('loading')
+//        $("#img"+$data_id).show()
         
         $.ajax({
            type:'post',
@@ -201,12 +197,15 @@ $(document).ready(function(){
            success: function(json) {
                $('#modal01').empty().append('<p>'+json.msg+'</p>');
                $('#myModal01').modal('show');
-               $("#img"+$data_id).hide();
+//               $("#img"+$data_id).hide();
+               $button.button('reset')
+
            },           
            error:function (xhr, ajaxOptions, thrownError){
                 $('#modal01').empty().append('<p>There was an error in removing the invitation.'+thrownError+'</p>');
                 $('#myModal01').modal('show');
-                $("#img"+$data_id).hide();
+//                $("#img"+$data_id).hide();
+                $button.button('reset')
            } 
         });
     });
